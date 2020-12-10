77f1b899968c56e8a9b9b1e2be9494f12619ece4
a.sidko <sidkoo@outlook.com>
2020-12-10 21:01:21 +0300

    Fix time format

--- code/api/time.php
+++ code/api/time.php
@@ -1,4 +1,8 @@
 <?php
 header('Content-Type: application/json');
 
-echo json_encode(["time" => time ( )]);
+$dt = new DateTime('2010-12-30 23:21:46');
+$dt->setTimezone(new DateTimeZone('UTC'));
+$time = $dt->format('Y-m-d\TH:i\Z');
+
+echo json_encode(["currentDateTime" => $time]);
